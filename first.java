278fa0c482b3e50da3a8d52100c11265e65bae9b
“[Shivam <rsprime74@gmail.com>
2024-01-19 19:16:58 +0530

    Done

--- first.java
+++ first.java
@@ -1,21 +1,32 @@
 public class first {
-    static boolean isPrime(int n)
+    // //static boolean isPrime(int n)
+    // {
+    //     if (n <= 1)
+    //     return false;
+    //     for (int i = 2; i < n; i++)
+    //     if (n%i==0)
+    //     return false;
+    
+    //     return true;
+    // }
+    public static boolean isPrime(int n)
     {
         if (n <= 1)
         return false;
-        for (int i = 2; i < n; i++)
-        if (n%i==0)
-        return false;
-    
-        return true;
+        for (int i = 2;i <= Math.sqrt(n);i++){
+        if( n % i==0){
+            return false;
+        }
+    }
+    return true;
     }
     public static void main(String[] args) {
-        if (isPrime(11)) {
-            System.out.println("true");
+        //if (isPrime(1)) {
+            System.out.println(isPrime(11));
             
-        }
-        else
-        System.out.println("false");
+        //}
+        //else
+        System.out.println(isPrime(12));
     }
 }
 
